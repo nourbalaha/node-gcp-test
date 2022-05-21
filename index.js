0e3651912d8cf1526259ec0ba1352579e3d77883
nourbalaha <nourbalaha0@gmail.com>
2022-05-21 22:50:09 +0900

    fixes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+"use strict";
+
 const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req,res) => {
-    res.send("<h2>It's working!");
+app.get("/", (req, res) => {
+  res.send("<h2>It's working!");
 });
 
 app.listen(PORT, () => {
-    console.log(`API is listening on port ${PORT}...`)
+  console.log(`API is listening on port ${PORT}...`);
 });
